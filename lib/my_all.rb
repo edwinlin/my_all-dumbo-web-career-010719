2ff5094b6b94c997a0cd66be5c4721ca2fcb8411
Edwin Lin <edwinlin@gmail.com>
2019-01-09 16:40:58 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0
   results = []
   while i < collection.length
-    false if !yield(collection[i])
+    return false if !yield(collection[i])
     i = i + 1
   end
 end
